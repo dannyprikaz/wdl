037eb5a50f21fd4487cb71206ed0f02378fbcb7d
dannyprikaz <dannyprikaz@gmail.com>
2018-08-27 14:05:19 -0700

    Create new players in dynamic list using AddPlayer component
    Resolves Ticket: WDL1

--- dashboard/components/table.js
+++ dashboard/components/table.js
@@ -3,14 +3,23 @@ class Table extends React.Component {
         super(props)
 
         this.state = {
-            players: ['Hannah', 'Lawrence', 'Robert', 'Alyssa']
+            players: []
         }
+        this.addPlayer = this.addPlayer.bind(this)
+    }
+    addPlayer(player) {
+      this.setState((state) => ({
+        players: state.players.concat([player])
+      }))
     }
     render() {
         return (
+          <div>
+            <AddPlayer addNew={this.addPlayer} />
             <div>
                 {this.state.players.map((player) => <Player name={player} />)}
             </div>
+          </div>
         )
     }
 }
