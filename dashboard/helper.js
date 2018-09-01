4707e3cf2f3c33cfd85aef6d9e5c1952c82681e2
dannyprikaz <dannyprikaz@gmail.com>
2018-08-31 18:28:27 -0700

    Display message in alert-graphic.html when 'goodPlay' received
    Resolves Ticket: WDL3

--- dashboard/helper.js
+++ dashboard/helper.js
@@ -1,3 +1,7 @@
 function newID () {
   return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
