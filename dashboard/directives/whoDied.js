836ccb27532d2e606c43d72bad62b385bd4454f7
dannyprikaz <dannyprikaz@gmail.com>
2018-10-04 19:52:50 -0600

    Display player names during who died game phase
    Resolves Ticket: WDL15

--- dashboard/directives/whoDied.js
+++ dashboard/directives/whoDied.js
@@ -6,12 +6,29 @@
 
 app.directive('whoDied', function() {
   var template = '<h2>Who died?</h2>' +
-             '<button ng-click="next()">Next</button>'
+                 '<div style="display:inline-block;margin:10px" ng-repeat="(key, player) in players">' +
+                 '<h3>{{player.name}}</h3>' +
+                 '</div>' +
+                 '<button ng-click="next()">Next</button>',
+
+      controller = ['$scope', function ($scope) {
+        $scope.players = {};
+
+        playersRep.on('change', (newValue, oldValue) => {
+          $scope.$apply();
+        });
+
+        NodeCG.waitForReplicants(playersRep).then(() => {
+          $scope.players = playersRep.value;
+          $scope.$apply();
+        });
+      }]
   return {
     scope: {
       next: '&'
     },
     restrict: 'E',
-    template: template
+    template: template,
+    controller: controller
   };
 });
