8f24f11216c33daefe5aa2f743b1cd2c0566d813
dannyprikaz <dannyprikaz@gmail.com>
2018-10-05 10:48:47 -0400

    Display which team(s) win(s) as a result of the players' deaths
    Resolves Ticket: WDL15

--- dashboard/directives/phases/whoDied.js
+++ dashboard/directives/phases/whoDied.js
@@ -8,7 +8,25 @@ app.directive('whoDied', function() {
   var controller = ['$scope', function ($scope) {
         $scope.players = {};
         $scope.rolesKilled = [];
+        $scope.winners = ['Werewolf', 'Tanner', 'Villager'];
         $scope.toggle = toggle;
+
+        $scope.victory = function(winner) {
+          var wolfKilled = $scope.rolesKilled.some(function(role) {
+            return role.includes('Werewolf');
+          });
+          var tannerKilled = $scope.rolesKilled.includes('Tanner');
+          var wasWolf = Object.entries($scope.players).some(function(entry) {
+            return entry[1].role.includes('Werewolf');
+          });
+          if (winner === 'Villager') {
+            return wolfKilled || (!wasWolf && $scope.rolesKilled.length == 0);
+          } else if (winner === 'Tanner') {
+            return tannerKilled;
+          } else if (winner === 'Werewolf') {
+            return !(wolfKilled || tannerKilled) && wasWolf;
+          }
+        };
 
         playersRep.on('change', (newValue, oldValue) => {
           $scope.$apply();
