41d06946ff705c02576c1955a75214efcaa567a2
dannyprikaz <dannyprikaz@gmail.com>
2018-10-07 19:19:57 -0400

    Add graphic for middle cards that displays role names
    Resolves Ticket: WDL13

--- dashboard/helper.js
+++ dashboard/helper.js
@@ -1,6 +1,12 @@
 function newID () {
   return (Date.now().toString(36) + Math.random().toString(36).substr(2, 5)).toUpperCase()
 }
+function endsWithNumber(string) {
+  return Boolean(Number(string[string.length-1]));
+}
+function noTrailingNumber(string) {
+  return endsWithNumber(string) ? string.slice(0, string.length-1): string;
+}
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
