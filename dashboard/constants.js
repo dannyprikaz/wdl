f48ea314a2c93ee6eeaee442cbb9d6a270f544d8
dannyprikaz <dannyprikaz@gmail.com>
2018-10-18 16:25:23 -0700

    Add dashboard panel for setting and starting the countdown timer
    Resolves Ticket: WDL20

--- dashboard/constants.js
+++ dashboard/constants.js
@@ -1,18 +1,19 @@
 const playersRep = nodecg.Replicant('playersRep');
 const activeRolesRep = nodecg.Replicant('activeRolesRep');
 const middleRolesRep = nodecg.Replicant('middleRolesRep');
-const safeToSet = NodeCG.waitForReplicants(playersRep, activeRolesRep, middleRolesRep);
+const countdownRep = nodecg.Replicant('countdownRep');
+const safeToSet = NodeCG.waitForReplicants(
+  playersRep, activeRolesRep, middleRolesRep, countdownRep
+);
+const streamStartMessage = 'The stream will begin shortly.'
 safeToSet.then(() => {
-  playersRep.value = (playersRep.value ? playersRep.value : {})
-  activeRolesRep.value = (activeRolesRep.value ? activeRolesRep.value : [])
-  middleRolesRep.value = (middleRolesRep.value ? middleRolesRep.value : [])
+  playersRep.value = (playersRep.value ? playersRep.value : {});
+  activeRolesRep.value = (activeRolesRep.value ? activeRolesRep.value : []);
+  middleRolesRep.value = (middleRolesRep.value ? middleRolesRep.value : []);
+  countdownRep.value = streamStartMessage;
 });
 const onuwRoles = [
-  'Werewolf1', 'Werewolf2', 'Villager1', 'Villager2', 'Villager3', 'Seer', 'Robber',
-  'Troublemaker', 'Tanner', 'Drunk', 'Hunter', 'Mason1', 'Mason2', 'Insomniac',
-  'Minion', 'Doppelganger'
+  'Werewolf1', 'Werewolf2', 'Villager1', 'Villager2', 'Villager3', 'Seer',
+  'Robber', 'Troublemaker', 'Tanner', 'Drunk', 'Hunter', 'Mason1', 'Mason2',
+  'Insomniac', 'Minion', 'Doppelganger'
 ];
-// const rolePhases = [
-//   {role: 'Troublemaker', directiveName: 'trouble-maker'},
-//   {role: 'Robber', directiveName: 'robber'}
-// ]
